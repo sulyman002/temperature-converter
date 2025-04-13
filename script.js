82e0bd1a661f75c0f216201ea8657593d88d75ea
sulyman002 <oyedelesulaiman@gmail.com>
2025-04-13 23:19:13 +0100

    general editing

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ function button() {
     const value = parseFloat(document.getElementById('input_value').value);
     const fromUnit = document.getElementById('fromUnit').value;
     const toUnit = document.getElementById('toUnit').value;
+    
 
     let result;
 
     if (fromUnit === toUnit) {
         result = value;
     } else if (fromUnit === 'C') {
-        result = toUnit === "F" ? (value * 9 / 5) + 32 : value + 273.15;
-    } else if (fromUnit === "F") {
+        result = toUnit === "D" ? (value * 9 / 5) + 32 : value + 273.15;
+    } else if (fromUnit === "D") {
         result = toUnit === "C" ? (value - 32) * 5 / 9 : ((value - 32) * 5 / 9) + 273.15;
     } else if (fromUnit === "K") {
         result = toUnit === "C" ? value - 273.15 : value - 273.15 * 9 / 5 + 32;
     }
 
-    myResult.value = `${result.toFixed(2)} °${toUnit}`;
+    myResult.value = ` ${value} °${fromUnit} is ${result.toFixed(2)} °${toUnit}`;
 
 }
